559f0a4e51dfe0a80fdd128ccaf1d184165f612e
Sandi Novakov <novakov.sandi@gmail.com>
2025-12-08 23:40:58 +0100

    added ibl

--- lua/cruxade/init.lua
+++ lua/cruxade/init.lua
@@ -5,5 +5,7 @@ vim.g.mapleader = " "
 require("cruxade.set")
 require("cruxade.lazy_init")
 
+-- this should all go somewhere else tbh -- 
 vim.cmd[[colorscheme tokyonight-night]]
+require("ibl").setup({scope = {enabled = false}}) --setup for indent-blankline
 
