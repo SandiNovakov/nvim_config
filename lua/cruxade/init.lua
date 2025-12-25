548e1112c090731e2905e25440eaf9c51f575526
Sandi Novakov <novakov.sandi@gmail.com>
2025-12-26 00:27:34 +0100

    added optional transparency

--- lua/cruxade/init.lua
+++ lua/cruxade/init.lua
@@ -6,3 +6,19 @@ require("cruxade.set")
 require("cruxade.lazy_init")
 require("cruxade.remap")
 require("cruxade.themes")
+
+vim.api.nvim_create_user_command("GiveUsEyes", function()
+	local groups = {
+		"Normal",
+		"NormalFloat",
+		"SignColumn",
+		"FoldColumn",
+		"LineNr",
+		"CursorLineNr",
+		"EndOfBuffer",
+	}
+
+	for _, g in ipairs(groups) do
+		vim.api.nvim_set_hl(0, g, { bg = "NONE" })
+	end
+end, {})
