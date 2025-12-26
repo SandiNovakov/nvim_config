1e40fd45a587ec8f477bcb0925bafc62c8ed50b8
Sandi Novakov <novakov.sandi@gmail.com>
2025-12-26 17:24:14 +0100

    fixed minor conflict with telescope and ffsq

--- lua/cruxade/lazy/telescope.lua
+++ lua/cruxade/lazy/telescope.lua
@@ -1,6 +1,7 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.2.0",
+	dependencies = { "nvim-lua/plenary.nvim" },
 
 	config = function()
 		require("telescope").setup({
@@ -8,18 +9,18 @@ return {
 			defaults = {
 				mappings = {
 					i = {
-						["<C-j>"] = require('telescope.actions').move_selection_next,
-						["<C-k>"] = require('telescope.actions').move_selection_previous,
-						["<C-l>"] = require('telescope.actions').select_default,
+						["<C-j>"] = require("telescope.actions").move_selection_next,
+						["<C-k>"] = require("telescope.actions").move_selection_previous,
+						["<C-l>"] = require("telescope.actions").select_default,
 					},
 				},
+			},
+		})
 
-			}})
-
-		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-	end
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+	end,
 }
