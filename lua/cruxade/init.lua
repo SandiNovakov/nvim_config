f1ccff228dd9226903f94c1424cec670936182f0
SandiNovakov <novakov.sandi@gmail.com>
2026-01-17 00:24:35 +0100

    removed files

--- lua/cruxade/init.lua
+++ lua/cruxade/init.lua
@@ -5,7 +5,6 @@ vim.g.mapleader = " "
 require("cruxade.set")
 require("cruxade.lazy_init")
 require("cruxade.remap")
-require("cruxade.themes")
 
 vim.api.nvim_create_user_command("GiveUsEyes", function()
 	local groups = {
